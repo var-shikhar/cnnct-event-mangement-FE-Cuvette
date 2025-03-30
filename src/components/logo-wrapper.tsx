a172d87bface7b41d41e024ccea943b94c1e6afa
var-shikhar <shikharvarshney10@gmail.com>
2025-03-30 11:18:11 +0530

    Fix: .env.example update

--- src/components/logo-wrapper.tsx
+++ src/components/logo-wrapper.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom"
 import LOGO from "../assets/logo.svg"
 import "./css/logo-wrapper.css"
 
 const LogoWrapper = () => {
   return (
-    <div className="logo-wrapper">
-      <img src={LOGO} alt="CNNCT" width={25} />
-      <span>CNNCT</span>
-    </div>
+    <Link to={"/"} className="logo-link">
+      <div className="logo-wrapper">
+        <img src={LOGO} alt="CNNCT" width={25} />
+        <span>CNNCT</span>
+      </div>
+    </Link>
   )
 }
 
